448915185de71f8b3d30d9a53285e62acb47c4d1
HarshitJuneja73 <96423083+HarshitJuneja73@users.noreply.github.com>
2022-04-07 02:19:47 +0530

    Update textDet.py

--- textDet.py
+++ textDet.py
@@ -1,57 +1,58 @@
-
 import cv2
 import pytesseract
 
-# Mention the installed location of Tesseract-OCR in your system. I use a mac, so it will most probably be compatible with other Mac OS devices
-pytesseract.pytesseract.tesseract_cmd = '/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages'
+def detector(file):
 
-# Reading the ad
-img = cv2.imread("/Users/harshit/Documents/Codes/AdEase/output1gfg.jpg")
+    # Mention the installed location of Tesseract-OCR in your system. I use a mac, so it will most probably be compatible with other Mac OS devices
+    pytesseract.pytesseract.tesseract_cmd = '/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages'
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # converting to grayscale
+    # Reading the ad
+    img = cv2.imread(file)
 
-
-ret, thresh1 = cv2.threshold(
-    gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-
-# Specify structure shape and kernel size.
-# Kernel size increases or decreases the area
-# of the rectangle to be detected.
-# A smaller value like (10, 10) will detect
-# each word instead of a sentence.
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # converting to grayscale
 
 
-dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+    ret, thresh1 = cv2.threshold(
+        gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+    # Specify structure shape and kernel size.
+    # Kernel size increases or decreases the area
+    # of the rectangle to be detected.
+    # A smaller value like (10, 10) will detect
+    # each word instead of a sentence.
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
 
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                       cv2.CHAIN_APPROX_NONE)
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
 
-im2 = img.copy()
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                        cv2.CHAIN_APPROX_NONE)
 
 
-file = open("recognized.txt", "w+")
-file.write("")
-file.close()
+    im2 = img.copy()
 
-# Looping through the identified contours
-# Then rectangular part is cropped and passed on
-# to pytesseract for extracting text from it
-# Extracted text is then written into the text file
-for cnt in contours:
-    x, y, w, h = cv2.boundingRect(cnt)
 
-    rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    file = open("recognized.txt", "w+")
+    file.write("")
+    file.close()
 
-    cropped = im2[y:y + h, x:x + w]
+    # Looping through the identified contours
+    # Then rectangular part is cropped and passed on
+    # to pytesseract for extracting text from it
+    # Extracted text is then written into the text file
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
 
-    file = open("recognized.txt", "a")
+        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    text = pytesseract.image_to_string(cropped)
+        cropped = im2[y:y + h, x:x + w]
 
-    file.write(text)
-    file.write("\n")
+        file = open("recognized.txt", "a")
 
-    file.close
+        text = pytesseract.image_to_string(cropped)
+
+        file.write(text)
+        file.write("\n")
+
+        file.close
